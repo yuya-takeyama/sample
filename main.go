f8f76155126d1f21af00f6feb59fc74cbbedd341
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-12-06 21:44:52 +0900

    Refactoring

--- main.go
+++ main.go
@@ -16,10 +16,11 @@ type cmdOptions struct {
 
 func main() {
 	opts := &cmdOptions{}
-	p := flags.NewParser(opts, flags.HelpFlag+flags.PrintErrors)
+	p := flags.NewParser(opts, flags.HelpFlag)
 	args, err := p.Parse()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %s\n", err.Error())
+		fmt.Fprint(os.Stderr, err.Error())
+		os.Exit(1)
 	}
 
 	rate := opts.Rate
@@ -29,30 +30,31 @@ func main() {
 	lineCh := make(chan []byte)
 	exitCh := make(chan bool)
 
-	go func(stdin io.Reader, args []string) {
-		if len(args) > 0 {
-			for _, filepath := range args {
-				file, err := os.Open(filepath)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "failed to open file: %s: %s", filepath, err.Error())
-					os.Exit(1)
-				}
-
-				readLines(file, lineCh)
-
-				if err = file.Close(); err != nil {
-					fmt.Fprintf(os.Stderr, "failed to close file: %s: %s", filepath, err.Error())
-					os.Exit(1)
-				}
-			}
-		} else {
-			readLines(stdin, lineCh)
-		}
-
-		exitCh <- true
-	}(os.Stdin, args)
-
+	go readInputs(os.Stdin, args, lineCh, exitCh)
 	printSampledLines(lineCh, exitCh, rate)
+}
+
+func readInputs(stdin io.Reader, args []string, lineCh chan []byte, exitCh chan bool) {
+	if len(args) > 0 {
+		for _, filepath := range args {
+			file, err := os.Open(filepath)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "failed to open file: %s\n", err.Error())
+				os.Exit(1)
+			}
+
+			readLines(file, lineCh)
+
+			if err = file.Close(); err != nil {
+				fmt.Fprintf(os.Stderr, "failed to close file: %s\n", err.Error())
+				os.Exit(1)
+			}
+		}
+	} else {
+		readLines(stdin, lineCh)
+	}
+
+	exitCh <- true
 }
 
 func readLines(file io.Reader, lineCh chan []byte) {
